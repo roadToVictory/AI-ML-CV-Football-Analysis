08a4d6594b53e408f118e612c72445214f65bd4e
roadToVictory <damian.koperstynski@gmail.com>
2024-08-12 23:48:41 +0200

    Step 2 - add CUDA to faster execute

--- yolo.py
+++ yolo.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
+import torch
 
 model = YOLO('yolov8x')
-
-results = model.predict('input_data/08fd33_4.mp4', save=True)
+print(torch.cuda.is_available())
+results = model.predict('input_data/08fd33_4.mp4', save=True, device='cuda')    #device=0
 
 print(results[0])
 print('=========================================')
